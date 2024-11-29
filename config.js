b66e12a1cfd8161f4062dea0bca98e6fb527a640
stark3693 <karanpatel2832005@gmail.com>
2024-11-29 19:46:49 +0530

    loveyou

---
+++ config.js
@@ -0,0 +1,23 @@
+// 霸都丶傲天 2019年12月24日
+
+var config = {
+    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+    // 每句话尽量不要超过15个字,不然展示效果可能不太好
+    texts: [
+        "Your smile lights up the darkest ", //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "day Chasing all my fears away", // 同上...
+        "I need your smile, don’t let it fade",
+        "It’s the sunshine when skies turn gray",
+        "Every glance, it feels like home,",
+        "With you, I’m never alone.",
+        "I need your smile, it makes me whole",
+        "It’s the rhythm within my soul.",
+        "Stay with me, let your laughter shine",
+        "Your smile’s the spark that lights my life.",
+        "Karan~~",
+    ],
+    wish: "Please Make a Smile ", //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+    // 时间的格式很重要哦，一定要是下面这个格式!!
+    // 年年年年-月月-日日 时时:分分:秒秒
+    // time: "2024-11-29 0:0:0", //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+};
